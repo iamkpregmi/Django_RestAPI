36181febdf91bc114180bd82071b544de8f23e93
Krishna Prasad Regmi <kpkrishna309@gmail.com>
2023-09-07 17:13:28 +0530

    Delete krishnaAPI directory

--- krishnaAPI/mainApp/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Company
-
-#Create Serializer here
-class CompanySerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model= Company
-        fields = "__all__"
